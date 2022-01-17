1fac072ec20e8c1ce8048e2ec888e9cfff0c3e8a
amacic121 <andrej.macic121@gmail.com>
2022-01-17 15:35:30 +0100

    Find user by username

--- routes/auth.js
+++ routes/auth.js
@@ -5,36 +5,37 @@ const res = require('express/lib/response');
 const { func } = require('joi');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const {User} = require('../models/user');
+const { User } = require('../models/user');
 
 
-router.post('/login', function(req, res) {
+router.post('/login', function (req, res) {
 
     let user = findUserByUsername(req, res);
 
-     const token = jwt.sign({user}, 'password');
-     res.json({
-         token: token
-     });
-});
-
-router.get('/hidden', ensureToken, function(req,res){
-    jwt.verify(req.token, 'password', function(err, data){
-if(err){
-    res.sendStatus(403);
-} else {
+    const token = jwt.sign({ user }, 'password');
     res.json({
-        text: 'this is password',
-        data: data
+        token: token
     });
-}
+
+});
+
+router.get('/hidden', ensureToken, function (req, res) {
+    jwt.verify(req.token, 'password', function (err, data) {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                text: 'this is password',
+                data: data
+            });
+        }
 
     });
 });
 
-function ensureToken(req,res,next){
+function ensureToken(req, res, next) {
     const bearerHeader = req.headers["autorizacija"];
-    if(typeof bearerHeader !== 'undefined'){
+    if (typeof bearerHeader !== 'undefined') {
         const bearer = bearerHeader.split(" ");
         const bearerToken = bearer[1];
         req.token = bearerToken;
@@ -44,10 +45,11 @@ function ensureToken(req,res,next){
     }
 }
 
-async function findUserByUsername(req, res){
-    let user = await User.findOne({username: req.body.username});
-    if(user === null){
-        throw new Error('User ne postoji');
+async function findUserByUsername(req, res) {
+    let user = await User.findOne({ username: req.body.username });
+    console.log(user);
+    if (user === null) {
+        console.log("Ne postoji takav user");
     }
     return user;
 }
