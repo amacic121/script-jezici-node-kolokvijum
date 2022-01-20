36ae9b7de57c4c259508bb4ace15b66793b60c77
amacic121 <andrej.macic121@gmail.com>
2022-01-20 04:27:32 +0100

    patch 0.0.2

--- models/task.js
+++ models/task.js
@@ -25,12 +25,15 @@ function validateTask(tasl){
     const schema = Joi.object({
         description: Joi.string().min(3).max(500).required(),
         typeOfTask: Joi.string().required(),
-        startingDate: Joi.string().min(6).max(255).required().email(),
-        dueDate: Joi.string().min(5).max(255).required(),
+        typeOfTask: Joi.string({ minDomainSegments: 5, tlds: { allow: ['Regular Task', 'Urgent Task' , 'Short Task' , 'Long Task','Other'] } }).required(),
+        startingDate: Joi.string().min(6).max(255).required().Date(),
+        dueDate: Joi.string().min(5).max(255).required().Date(),
+        
     });
     return schema.validate(task);
 }
 
 const Task = mongoose.model('Task', taskSchema);
 
+// exporting the Schema
 module.exports = Task;
